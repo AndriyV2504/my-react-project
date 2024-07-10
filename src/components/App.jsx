ff38438482f5530fae3943a64ceff48ac2e71f27
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-10 23:48:33 +0300

    add_formik_feedbackform

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,31 @@ import LoginForm from "./LoginForm/LoginForm";
 import SearchBar from "./SearchBar/SearchBar";
 import LangSwitcher from "./LangSwitcher/LangSwitcher";
 import { useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 const App = () => {
+  const initialValues = {
+    username: "",
+    email: "",
+  };
+
+  const FeedbackForm = () => {
+    const handleSubmit = (values, actions) => {
+      console.log(values);
+      actions.resetForm();
+    };
+
+    return (
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field type="text" name="username" />
+          <Field type="email" name="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    );
+  };
+
   const handleLogin = (userData) => {
     console.log(userData);
   };
@@ -13,6 +36,9 @@ const App = () => {
 
   return (
     <>
+      <p>FeedbackForm</p>
+      <FeedbackForm />
+      {/* ============================================= */}
       <p>Selected language: {lang}</p>
       <LangSwitcher value={lang} onSelect={setLang} />
       {/* ============================================= */}
---
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -0,0 +1,5 @@
+const FeedbackForm = () => {
+  return <div></div>;
+};
+
+export default FeedbackForm;
